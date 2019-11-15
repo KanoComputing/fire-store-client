0afd54c79e24064bde8ba2469b3ec69b7bfe53da
Radek Pazdera <radek@kano.me>
2019-11-15 15:31:56 +0000

    Upload implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,91 @@
-export const lol = 5;
+import fetch, { RequestInit, RequestInfo, Response } from 'node-fetch';
+import URLSearchParams from 'url-search-params';
+import { createReadStream } from 'fs';
+
+export class FireStoreClient {
+    private clientId: string;
+    private clientSecret: string;
+    private grantType = 'client_credentials';
+    private scope = 'appstore::apps:readwrite';
+
+    private AUTH_URL = 'https://api.amazon.com/auth/o2/token';
+    private BASE_URL = 'https://developer.amazon.com/api/appstore';
+
+    token: string;
+    expiry: number;
+
+    /* Testing */
+    fetch: (url: RequestInfo, init?: RequestInit) => Promise<any> = fetch;
+
+    constructor(clientId: string, clientSecret: string, grantType?: string, scope?: string) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+
+        if (grantType) {
+            this.grantType = grantType;
+        }
+
+        if (scope) {
+            this.scope = grantType;
+        }
+    }
+
+    async authenticate() {
+        const body = new URLSearchParams();
+        body.set('client_id', this.clientId);
+        body.set('client_secret', this.clientSecret);
+        body.set('grant_type', this.grantType);
+        body.set('scope', this.scope);
+
+        const res = await this.fetch(this.AUTH_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'pplication/x-www-form-urlencoded',
+            },
+            body,
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(`Authentication failed: ${data.error_description}`);
+        }
+
+        this.token = data.access_token;
+        this.expiry = Math.floor(Date.now() / 1000) + data.expires_in;
+    }
+
+    async uploadApk(appId: string, apkPath: string) {
+        if (!this.token || this.expiry > Date.now()) {
+            await this.authenticate();
+        }
+
+        const editRes = await this.fetch(`${this.BASE_URL}/v1/applications/${appId}/edits`, {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${this.token}`
+            }
+        });
+
+        if (!editRes.ok) {
+            throw new Error('Failed to create an edit');
+        }
+
+        const { id: editId } = await editRes.json();
+
+        const uploadRes = await this.fetch(`${this.BASE_URL}/v1/applications/${appId}/edits/${editId}/apks/upload`, {
+            headers: {
+                method: 'POST',
+                Authorization: `Bearer ${this.token}`,
+                'Content-Type': 'application/vnd.android.package-archive',
+            },
+            body: createReadStream(apkPath),
+        });
+
+        if (!uploadRes.ok) {
+            throw new Error('Uploading APK failed');
+        }
+
+        return await uploadRes.json();
+    }
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,79 @@
 import tap from 'tap';
-import { lol } from '../src';
+import { RequestInfo, RequestInit } from 'node-fetch';
+import { FireStoreClient } from '../src';
+import { ReadStream } from 'fs';
 
 tap.test(async t => {
-    await t.test(async t => {
-        t.equal(lol, 5);
+    const client = new FireStoreClient('test_id', 'test_secret');
+    const originalFetch = client.fetch;
+
+    t.afterEach(t => {
+        client.fetch = originalFetch;
     });
+
+    await t.test('Authenticate', async t => {
+        client.fetch = (url: RequestInfo, init?: RequestInit) => {
+            t.type(init.body, URLSearchParams);
+
+            const body = init.body as URLSearchParams;
+            t.equal(body.get('client_id'), 'test_id');
+            t.equal(body.get('client_secret'), 'test_secret');
+            t.equal(body.get('grant_type'), 'client_credentials');
+            t.equal(body.get('scope'), 'appstore::apps:readwrite');
+
+            t.equal(url, 'https://api.amazon.com/auth/o2/token');
+
+            return new Promise(resolve => resolve({
+                status: 200,
+                ok: true,
+                json: () => Promise.resolve({ access_token: 'test_token', expires_in: 1234 })
+            }));
+        };
+
+        await client.authenticate();
+
+        t.equal(client.token, 'test_token');
+        t.ok(Math.abs(client.expiry - Date.now() / 1000 - 1234) < 10);
+
+        t.done();
+    });
+
+    await t.test('Upload apk', async t => {
+        let editCalled = false;
+        let uploadCalled = false;
+
+        client.fetch = (url: RequestInfo, init?: RequestInit) => {
+            let res;
+
+            t.equal((init.headers as any).Authorization, 'Bearer test_token');
+
+            if ((url as string).match(/upload$/)) {
+                t.type(init.body, ReadStream);
+
+                t.ok((url as string).match(/test_edit_id/));
+                t.equal((init.headers as any)['Content-Type'], 'application/vnd.android.package-archive');
+                uploadCalled = true;
+
+                res = { test: 'upload' };
+            } else {
+                t.notOk(init.body);
+                editCalled = true;
+                res = { id: 'test_edit_id' };
+            }
+
+            return new Promise(resolve => resolve({
+                status: 200,
+                ok: true,
+                json: () => Promise.resolve(res),
+            }));
+        };
+
+        const res = await client.uploadApk('test.app.id', 'src/index.ts');
+        t.equal(res.test, 'upload');
+        t.ok(editCalled);
+        t.ok(uploadCalled);
+
+        t.done();
+    });
+
 });
